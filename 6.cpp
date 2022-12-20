74b89cca77c4d9bb2aec29377c32f1ff1fe4f831
DaddyWarcrimes <4444646+DaddyWarcrimes@users.noreply.github.com>
2022-12-19 18:11:42 -0600

    day 6 part 2

--- 6.cpp
+++ 6.cpp
@@ -4,6 +4,7 @@
 
 int main(void){
 
+	int partSize = 14;
 	std::ifstream infile;
 	infile.open("6.txt");
 	if(!infile){
@@ -14,12 +15,29 @@ int main(void){
 	}
 	std::string line;
 
-	while(!infile.eof()){
-		getline(infile,line);
-		std::cout << line << std::endl;		
-	}
-	for(int i = 0; i < line.length() / 4 ; i++){
-		std::cout << line.substr(i * 4, 4) << std::endl;
+	getline(infile,line);
+	for(int i = 0; i < line.length() - partSize; i++){
+		std::string part = line.substr(i , partSize);
+		std::cout << part << "  ";
+		for(int j = 0; j < partSize - 1; j++){
+			std::string car = part.substr(j,1);
+			std::string cdr = part.substr(j + 1, part.length() - j - 1);
+			int found = cdr.find(car);
+			std::cout << "|" << car << "-" << cdr << ":" << found;
+//			std::cout << " | " << part.substr(i,1) << "-" << part.substr(i + 1, part.length() - i - 1);
+			if(found != -1){
+				break;
+			}
+			else if(j == partSize - 2){
+				std::cout << std::endl << i + partSize << " - Marker: " << part << std::endl;
+				return 0;
+			}
+			else{
+				continue;
+			}
+		}
+		std::cout << std::endl;
+		
 	}
 	
 }
