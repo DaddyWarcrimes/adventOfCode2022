7d887cfd54d25941558ca8326048f1c0696201f1
DaddyWarcrimes <4444646+DaddyWarcrimes@users.noreply.github.com>
2022-12-21 11:39:02 -0600

    Day 9 start

---
+++ 9.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(void){
+
+	std::ifstream infile;
+	infile.open("9.txt");
+	if(!infile){
+		printf("failed to open\n");
+	}
+	else{
+		printf("file opened\n");
+	}
+	std::string line;
+
+	while(!infile.eof()){
+		getline(infile,line);
+		std::cout << line << std::endl;		
+		
+	}
+}
