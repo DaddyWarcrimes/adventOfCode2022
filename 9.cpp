e881b0592e97d4aef3f617123b98ee71d2471b8e
DaddyWarcrimes <4444646+DaddyWarcrimes@users.noreply.github.com>
2022-12-22 01:38:12 -0600

    determine the size of the field

--- 9.cpp
+++ 9.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 
+
 int main(void){
 
 	std::ifstream infile;
@@ -13,10 +14,49 @@ int main(void){
 		printf("file opened\n");
 	}
 	std::string line;
+	int hH = 0;
+	int hV = 0;
+	int maxH = 0;
+	int minH = 0;
+	int maxV = 0;
+	int minV = 0;
 
 	while(!infile.eof()){
 		getline(infile,line);
-		std::cout << line << std::endl;		
-		
+		if(line.length() < 3){
+		continue;
+		}
+		char dir = line.at(0);
+		int q = stoi(line.substr(2,line.length() ));
+		std::cout << dir << " | " << q << std::endl;
+		switch(dir){
+			case 'R':
+				hH += q;
+				if(hH > maxH){
+					maxH = hH;
+				}
+				break;
+			case 'L':
+				hH -=q;
+				if(hH < minH){
+					minH = hH;
+				}
+				break;
+			case 'U':
+				hV -=q;
+				if(hV < minV){
+					minV = hV;
+				}
+				break;
+			case 'D':
+				hV +=q;
+				if(hV > maxV){
+					maxV = hV;
+				}
+				break;
+			default:
+				break;
+		}
 	}
+	printf("%d %d %d %d \n", maxH, minH, maxV, minV);
 }
